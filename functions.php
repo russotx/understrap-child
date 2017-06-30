25dd8eb5f1ff3ebe4681675757f4100f63d97b00
Daniel Russo <Daniel@Daniels-MacBook-Pro.local>
2017-06-30 16:39:44 -0500

    cleaning up header and footer, changed default fonts in sass

--- functions.php
+++ functions.php
@@ -19,3 +19,7 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
 }
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
