ba855660cc2a8795d68d0c94fb14979b971b84d8
Daniel Russo <Daniel@Daniels-MacBook-Pro.local>
2017-07-01 01:42:35 -0500

    social buttons added

--- functions.php
+++ functions.php
@@ -23,3 +23,6 @@ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
